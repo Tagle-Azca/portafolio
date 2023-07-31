ae748c0b7ac794507558d31c98131cee8b124747
Tagle-Azca <gtaglea@hotmail.com>
2023-07-31 00:16:47 -0600

    aportaciones a cards e creacion de otras slides

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -4,6 +4,7 @@ import backgroundImage from '../assets/IMG/IMG_1936.JPG';
 import Graduate from '../assets/IMG/graduate.jpg';
 
 export default function Cards() {
+
   const carta1Style = {
     width: '350px',
     height: '500px',
@@ -23,7 +24,7 @@ export default function Cards() {
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     opacity: 1,
-    transition: 'opacity 0.3s ease', // Agrega una transición suave de opacidad
+    transition: 'opacity 0.3s ease', 
   };
   
   const carta3Style = {
@@ -43,30 +44,34 @@ export default function Cards() {
   const [isHoveringCard3, setIsHoveringCard3] = useState(false);
 
   return (
+
     <div>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <div
-            style={{ ...carta1Style, opacity: isHoveringCard1 ? 0.6 : 1 }}
+            style={{ ...carta1Style, opacity: isHoveringCard1 ? 0.4 : 1 }}
             onMouseEnter={() => setIsHoveringCard1(true)}
             onMouseLeave={() => setIsHoveringCard1(false)}
           />
         </Grid>
         <Grid item xs={4}>
+
           <div
-            style={{ ...carta2Style, opacity: isHoveringCard2 ? 0.6 : 1 }}
+            style={{ ...carta2Style, opacity: isHoveringCard2 ? 0.4 : 1 }}
             onMouseEnter={() => setIsHoveringCard2(true)}
             onMouseLeave={() => setIsHoveringCard2(false)}
           />
+          
         </Grid>
         <Grid item xs={1}>
           <div
-            style={{ ...carta3Style, opacity: isHoveringCard3 ? 0.6 : 1 }}
+            style={{ ...carta3Style, opacity: isHoveringCard3 ? 0.4 : 1 }}
             onMouseEnter={() => setIsHoveringCard3(true)}
             onMouseLeave={() => setIsHoveringCard3(false)}
           />
         </Grid>
       </Grid>
     </div>
+    
   );
 }
