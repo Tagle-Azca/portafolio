cc4c11dd52ba22d4c04c4f9dc0a5a441c4cb9266
Tagle-Azca <gtaglea@hotmail.com>
2023-08-15 22:12:31 -0600

    15 agosto, sintaxis

--- src/Pages/Start.jsx
+++ src/Pages/Start.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import AnimatedIcons from "../Components/AnimatedIcons";
 import Letras from "../Components/Letras";
 import Background from '../Components/AnimatedBack';
-import ArrowDown from '../Components/ArrowDown'
-import Cards from '../Components/Cards'
+import ArrowDown from '../Components/ArrowDown';
 import SpeedDial from '../Components/SpeedDial'
 import Head from "../Components/Head";
 import SideBar from '../Components/SideBar';
@@ -38,7 +37,7 @@ const letrasContainerStyle = {
 };
 
 const mainContainerStyle = {
-    minHeight: "180vh", // Altura mínima del 100% del viewport
+    minHeight: "100vh", // Altura mínima del 100% del viewport
 };
 
 class Start extends React.Component {
@@ -48,27 +47,24 @@ class Start extends React.Component {
                 <div className="Sidebar" style={{ position: 'fixed', zIndex: 1, color:'#fff', top:'10' }}>
                     <SideBar/>
                 </div>
-                        <Background style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} />
-                                    <main style={mainContainerStyle}>
-                                            
-                                            <div id="inicio" style={{ ...mainContainerStyle, position: 'relative' }}>
+                <main style={mainContainerStyle}>
+                <Background style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} />
+                    
+                        <div id="inicio" style={{ ...mainContainerStyle, position: 'relative' }}>
+                            <div style={{ position: 'fixed', top: 10, right: '41%', transform: 'translateX(-50%)', zIndex: 1 }}>
+                                <div className="Iconos" style={iconContainerStyle}>
+                                    <AnimatedIcons />   
+                                </div>
+                            </div>
                                     
-                                    <div style={{ position: 'fixed', top: 10, right: '41%', transform: 'translateX(-50%)', zIndex: 1 }}>
+                            {/* Contenedor para el dial */}
+                            <div style={{ position: 'fixed', top: 0, right: '1%', transform: 'translateX(-50%)', zIndex: 1 }}>
+                                <div className="Dial" style={Dial}>
+                                    <SpeedDial/>
+                                </div>
+                            </div>
                                     
-                                    <div className="Iconos" style={iconContainerStyle}>
-                                        <AnimatedIcons />   
-                                    </div>
-                                        
-                                    </div>
-                                    
-                                    {/* Contenedor para el dial */}
-                                    <div style={{ position: 'fixed', top: 0, right: '1%', transform: 'translateX(-50%)', zIndex: 1 }}>
-                                        <div className="Dial" style={Dial}>
-                                            <SpeedDial/>
-                                        </div>
-                                    </div>
-                                    
-                        <Background />
+                <Background />
                         
 
                         <div className="Nombre" style={{ ...letrasContainerStyle, marginTop: '20%' }}>
@@ -82,11 +78,6 @@ class Start extends React.Component {
 
                     <div id="head" style={{ position: 'relative' }}>
                         <Head />
-                    </div>
-
-                    {/* Resto del contenido de tu página */}
-                    <div className="CardsSection" style={{ marginTop: '5vh', marginLeft: '0%', position: 'relative' }}>
-                        <Cards />
                     </div>
                 </main>
             </div>
